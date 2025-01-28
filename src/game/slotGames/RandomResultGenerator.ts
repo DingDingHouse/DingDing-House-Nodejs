b3dbc80471ec30a2fed49d69c5cdf1dbf49ab7f4
TrippyRahul <rahulrawat.trippybug@gmail.com>
2025-01-28 12:30:15 +0530

    ERR:EGT (Bonus random number errors fixed)

---
+++ src/game/slotGames/RandomResultGenerator.ts
@@ -0,0 +1,36 @@
+import { generateRandomNumber, generatetrueRandomNumber } from "../Utils/gameUtils";
+
+export class RandomResultGenerator {
+  constructor(current) {
+    let matrix: string[][] = [];
+    for (let x = 0; x < current.settings.currentGamedata.matrix.x; x++) {
+      const startPosition = this.getRandomIndex((current.settings.reels[x].length - 1));
+      for (let y = 0; y < current.settings.currentGamedata.matrix.y; y++) {
+        if (!matrix[y]) matrix[y] = [];
+        matrix[y][x] = current.settings.reels[x][(startPosition + y) % current.settings.reels[x].length];
+      }
+    }
+
+
+    // matrix.pop();
+    // matrix.pop();
+    // matrix.pop();
+    // matrix.push(['11', '11', '11', '11', '11'])
+    // matrix.push(['5', '3', '2', '1', '1'])
+    // matrix.push(['5', '3', '1', '5', '1'])
+    matrix.forEach(row => console.log(row.join(' ')));
+    current.settings.resultReelIndex = matrix;
+    current.settings.resultSymbolMatrix = matrix;
+
+  }
+  getRandomIndex(maxValue: number): number {
+    let seed = Date.now() + Math.random() * 1000;
+
+
+    // return Math.floor(generateRandomNumber(seed, (maxValue + 1)));
+    return Math.floor(generatetrueRandomNumber(maxValue + 1));
+
+  }
+
+}
+
