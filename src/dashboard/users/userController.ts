2803f318f18637438dda96fb88bc3d8743563ac0
TrippyRahul <129256734+TrippyRahul@users.noreply.github.com>
2025-01-30 15:53:16 +0530

    Update userController.ts

--- src/dashboard/users/userController.ts
+++ src/dashboard/users/userController.ts
@@ -173,11 +173,11 @@ export class UserController {
       const token = jwt.sign(
         { id: user._id, username: user.username, role: user.role },
         config.jwtSecret!,
-        { expiresIn: "7d" }
+        { expiresIn: "1m" }
       );
 
       res.cookie("userToken", token, {
-        maxAge: 1000 * 60 * 60 * 24 * 7,
+        maxAge: 1000 * 60,
         httpOnly: true,
         sameSite: "none",
       });
